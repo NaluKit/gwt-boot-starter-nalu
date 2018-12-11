ba2fc59b69fbfa23322efee2095228057ec1bc0f
Frank Hossfeld <frank.hossfeld@googlemail.com>
2018-12-11 17:17:11 +0100

    Fixed an issue with generating project

--- src/main/java/com/github/nalukit/gwtbootstarternalu/client/Version.java
+++ src/main/java/com/github/nalukit/gwtbootstarternalu/client/Version.java
@@ -2,6 +2,6 @@ package com.github.nalukit.gwtbootstarternalu.client;
 
 public class Version {
 
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
 
 }
