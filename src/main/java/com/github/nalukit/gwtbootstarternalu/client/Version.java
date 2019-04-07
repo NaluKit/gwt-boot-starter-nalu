8e5d6ad322b167b401915b3684302db89300e9dd
Frank Hossfeld <frank.hossfeld@googlemail.com>
2019-04-07 09:52:02 +0200

    update version

--- src/main/java/com/github/nalukit/gwtbootstarternalu/client/Version.java
+++ src/main/java/com/github/nalukit/gwtbootstarternalu/client/Version.java
@@ -2,6 +2,6 @@ package com.github.nalukit.gwtbootstarternalu.client;
 
 public class Version {
 
-  public static final String VERSION = "1.2.0.0003";
+  public static final String VERSION = "1.2.0.0004";
 
 }
